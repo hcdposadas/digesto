e3a13a4dbf3310c45e84202463a442a2a50ca9d3
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2019-12-13 09:44:38 -0300

    add cargo to form web

---
+++ src/Migrations/Version20191213123803.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191213123803 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE web_digesto_texto ADD vice_presidente_primero_cargo VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE web_digesto_texto ADD vice_presidente_segundo_cargo VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE web_digesto_texto ADD director_cargo VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE web_digesto_texto DROP vice_presidente_primero_cargo');
+        $this->addSql('ALTER TABLE web_digesto_texto DROP vice_presidente_segundo_cargo');
+        $this->addSql('ALTER TABLE web_digesto_texto DROP director_cargo');
+    }
+}
